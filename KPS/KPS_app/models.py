60d55c028cef12af23d95e53c6899c76b534e3a9
Benjamin Riddell <benjamin.riddell@hotmail.com>
2015-05-19 20:31:05 +1200

    Updated the .gitignore file so that it ignores eclipse workspace files. Made some changes to the models concerning some simplifications and Django shortcuts like a multiple choice field and also uses some inheritance. I also made some changes to some of the field locations so check the class diagram for these changes.

--- KPS/KPS_app/models.py
+++ KPS/KPS_app/models.py
@@ -3,45 +3,63 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+PRIORITIES = (
+    (0, 'Land'),
+    (1, 'Sea'),
+    (2, 'Air'),
+)
+DAYS = (
+    (0, 'Monday'),
+    (1, 'Tuesday'),
+    (2, 'Wednesday'),
+    (3, 'Thursday'),
+    (4, 'Friday'),
+    (5, 'Saturday'),
+    (6, 'Sunday'),
+)
 
 class City(models.Model):
     city_name = models.CharField(max_length=200)
+
     def __str__(self):
         return self.city_name
 
+class Company(models.Model):
+    company_name = models.CharField(max_length=200)
+
 class BusinessEvent(models.Model):
-    recordedTime = models.DateTimeField('recorded time')
+    recorded_time = models.DateTimeField('recorded time', auto_now_add=True)
     to_city = models.ForeignKey(City)
+    from_city = models.ForeignKey(City)
+    priority = models.CharField(choices=PRIORITIES)
 
-    def asXML():
-      return "foo"
+    def asXML(self):
+        return "foo"
+    
+    class Meta:
+        abstract = True
 
-class MailDelivery(models.Model):
-    weight = models.IntegerField()
-    volume = models.IntegerField()
-    priority = models.CharField(max_length=200)
-    day = models.CharField(max_length=200)
+class MailDelivery(BusinessEvent):
+    weight = models.IntegerField('weight in grams')
+    volume = models.IntegerField('volume in cubic centimeter')
 
-class TransportCostUpdate(models.Model):
+class TransportCostUpdate(BusinessEvent):
+    company = models.ForeignKey(Company)
+    weight_cost = models.IntegerField('cost per gram')
+    volume_cost = models.IntegerField('cost per cubic centimeter')
+    max_weight = models.IntegerField('maximum weight in grams')
+    max_volume = models.IntegerField('maximum volume in cubic centimeters')
+    duration = models.IntegerField('duration of trip in hours')
+    frequency = models.IntegerField('number of hours between each departure')
+    day = models.CharField('day of the week the transport departs', choices=DAYS)
+    is_active = models.BooleanField('if the model is currently active')
+
+class PriceUpdate(BusinessEvent):
+    weight_cost = models.IntegerField('cost per gram')
+    volume_cost = models.IntegerField('cost per cubic centimeter')
+
+class TransportDiscontinued(BusinessEvent):
     company = models.CharField(max_length=200)
-    transport_type = models.CharField(max_length=200)
-    weight_cost = models.IntegerField()
-    volume_cost = models.IntegerField()
-    max_weight = models.IntegerField()
-    max_volume = models.IntegerField()
-    duration = models.IntegerField()
-    frequency = models.IntegerField()
-    day = models.CharField(max_length=200)
-    is_active = models.BooleanField()
-
-class PriceUpdate():
-    priority = models.CharField(max_length=200)
-    weight_cost = models.IntegerField()
-    volume_cost = models.IntegerField()
-
-class TransportDiscontinued():
-    company = models.CharField(max_length=200)
-    transport_discontinued_type = models.CharField(max_length=200)
 
 
 
